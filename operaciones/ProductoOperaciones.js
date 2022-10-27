80b050197a73c9bdeaf168e1152bb56028ccfaca
Eddie Quintero <92959645+Quineddie@users.noreply.github.com>
2022-10-27 18:18:10 -0500

    Update ProductoOperaciones.js

--- operaciones/ProductoOperaciones.js
+++ operaciones/ProductoOperaciones.js
@@ -12,4 +12,26 @@ productoOperaciones.crearProducto = async (req, res)=>{
         res.status(400).send("Mala petición. "+error);
     }
 }
+
+productoOperaciones.modificarProducto = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const body = req.body;
+        const datosActualizar = {
+            tipo: body.tipo,
+            precio: body.precio,
+            presentacion: body.presentacion,
+            
+        }
+        const productoActualizado = await productoModelo.findByIdAndUpdate(id, datosActualizar, { new : true });
+        if (productoActualizado != null) {
+            res.status(200).send(productoActualizado);
+        }
+        else {
+            res.status(404).send("No hay datos");
+        }
+    } catch (error) {
+        res.status(400).send("Mala petición. "+error);
+    }
+}
 module.exports = productoOperaciones;
