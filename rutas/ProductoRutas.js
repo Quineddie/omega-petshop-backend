2a7fb3a0647caa137c89bf0f405341e3b7f6b28e
Eddie Quintero <92959645+Quineddie@users.noreply.github.com>
2022-10-27 19:02:30 -0500

    Update ProductoRutas.js

--- rutas/ProductoRutas.js
+++ rutas/ProductoRutas.js
@@ -2,5 +2,6 @@ const productoOperaciones=require("../operaciones/ProductoOperaciones");
 const router = require("express").Router();
 
 router.post("/", productoOperaciones.crearProducto);
+router.put("/:id", productoOperaciones.modificarProducto);
 
 module.exports = router;
